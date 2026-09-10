88c168ad9513e2dc08a721e7f3ae35ce37b7ab1b
GeorgiiGeorgiev <dontes2007@ukr.net>
2026-09-10 17:44:55 +0300

    Refact trait for models

--- app/Models/Import.php
+++ app/Models/Import.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\Concerns\HasPublicUuid;
+use App\Models\Traits\HasPublicUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
